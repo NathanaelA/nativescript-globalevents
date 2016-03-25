15c11cd04475f7d2389d7f094c6292ff98664a16
Nathanael Anderson <nathan@master-technology.com>
2016-03-25 15:37:07 -0600

    First Commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# nativescript-globalevents
+A NativeScript plugin to deal with Global events
+
+## License
+
+This is released under the MIT License, meaning you are free to include this in any type of program -- However for entities that need a support contract, changes, enhancements and/or a commercial license please contact me (nathan@master-technology.com).
+
+
+## Installation 
+
+tns plugin add nativescript-globalevents
+
+
+## Usage
+
+To use the module you just `require()` it:
+
+```js
+require( "nativescript-globalevents" );
+```
+
+Notice: You do NOT need to keep a reference to it; and you only need to load it once.
+
+It will automatically attach its methods to all the proper classes in the NativeScript library making it act as if they are built in.
+
+What this does is automatically add support to allow you to setup events on all the page events globally.  For example; if you need a pageLoaded event to occur on every page; you can use this library to get a global pageLoaded event.
+
+Events currently on the **Page** that are supported are:
+- Page.navigatingToEvent
+- Page.navigatedToEvent
+- Page.navigatingFromEvent
+- Page.navigatedFromEvent
+- Page.shownModallyEvent
+- Page.showingModallyEvent
+- View.loaded
+- View.unloaded
+
+If you have any other global events you would like supported please open an issue on the github issue tracker and I will investigate its relevance.
+
+## You ask, how exactly does this help?
+Well, my usage is I have several plugins that need to be notified when a new page is loaded to allow custom handling to occur automatically; however it could be useful for your own app if you need to do something globally when every page is loaded or unloaded.
+
+```js
+var Page = require('ui/page').Page;
+Page.on(Page.navigatingTo, function() { console.log("We have navigated to a new page!"); });
+```
+
+## Why use this?
+If you need to track globally an event and you don't want to put a handler in each page.xml/page.js file.
+
+
+### Static Page class now has four new STATIC functions
+
+#### Page.on / Page.addEventHandler (eventName, callback[, thisArg])
+#### Page.off / Page.removeEventHandler (eventName[[, callback], thisArg])
+
+If thisArg is null/undefined it will use the current page's "this".
+If callback is null on the off/removePageHandler it will remove ALL handlers.
---
+++ globalevents.js
@@ -0,0 +1,172 @@
+/**********************************************************************************
+ * (c) 2016, Master Technology
+ * Licensed under the MIT license or contact me for a Support or Commercial License
+ *
+ * I do contract work in most languages, so let me solve your problems!
+ *
+ * Any questions please feel free to email me or put a issue up on the github repo
+ * Version 1.0.0                                      Nathan@master-technology.com
+ *********************************************************************************/
+"use strict";
+
+/* jshint camelcase: false, nodejs: true */
+/* global android */
+
+var Page = require('ui/page').Page;
+var View = require('ui/core/view').View;
+
+
+// If NS has added this ability to core; we want our plugin to gracefully stop working without doing anything...
+if (Page.on || Page.addEventListener) {
+    console.log("NativeScript-globalevents is disabled; functionality appears to be already present!");
+    return;
+}
+
+// Setup the original events tracking
+var events = {};
+
+// Setup our event trackers
+var eventHandlers = {};
+eventHandlers[Page.navigatingToEvent] = [];
+eventHandlers[Page.navigatedToEvent] = [];
+eventHandlers[Page.navigatingFromEvent] = [];
+eventHandlers[Page.navigatedFromEvent] = [];
+eventHandlers[Page.shownModallyEvent] = [];
+eventHandlers[Page.showingModallyEvent] = [];
+eventHandlers[View.loaded] = [];
+eventHandlers[View.unloaded] = [];
+
+// The event handler names
+var eventNames = {};
+eventNames[Page.navigatedToEvent] = 'onNavigatedTo';
+eventNames[Page.navigatingToEvent] = 'onNavigatingTo';
+eventNames[Page.navigatedFromEvent] = 'onNavigatedFrom';
+eventNames[Page.navigatingFromEvent] = 'onNavigatingFrom';
+eventNames[Page.shownModallyEvent] = '_raiseShownModallyEvent';
+eventNames[Page.showingModallyEvent] = '_raiseShowingModallyEvent';
+eventNames[View.loaded] = 'onLoaded';
+eventNames[View.unloaded] =  'onUnloaded';
+
+
+/**
+ * Setup a static on/addEventLister on the Page object
+ * @type {Page.addEventListener}
+ */
+Page.on = Page.addEventListener = function(event, callback, thisArg) {
+    if (typeof eventHandlers[event] === 'undefined') {
+        throw new Error("This global page event "+event+" does not exist, or is currently unsupported");
+    }
+    if (typeof callback !== 'function') {
+        throw new Error("Callback should be a function!");
+    }
+
+    // Dynamically hijack the event; so that we don't bother hijacking any events that aren't used
+    if (eventHandlers[event].length === 0) {
+        hijackEvent(event);
+    }
+    eventHandlers[event].push({callback: callback, thisArg: thisArg});
+};
+
+/**
+ * Setup a static off/removeEventLister
+ * @type {Page.removeEventListener}
+ */
+Page.off = Page.removeEventListener = function(event, callback, thisArg) {
+    if (typeof eventHandlers[event] === "undefined") {
+        throw new Error("This global page event "+event+" does not exist.");
+    }
+    var orgLength = eventHandlers[event].length;
+    if (callback) {
+        var index = indexOfListener(eventHandlers[event], callback, thisArg);
+        if (index >= 0) {
+            eventHandlers[event].splice(index, 1);
+        }
+    } else {
+        eventHandlers[event] = [];
+    }
+    if (eventHandlers[event].length === 0 && orgLength !== 0) {
+        restoreEvent(event);
+    }
+};
+
+
+/**
+ * Used to dynamically hijack the event
+ * @param event
+ */
+function hijackEvent(event) {
+    var handlerName = eventNames[event];
+    console.log("Handler", handlerName, event);
+
+    if (typeof Page.prototype[handlerName] === 'function') {
+        events[event] = Page.prototype[handlerName];
+    } else if (typeof View.prototype[handlerName] === 'function') {
+        events[event] = View.prototype[handlerName];
+    } else {
+        throw new Error("NativeScript-globalEvents is unable to find the proper event to attach to.  Please alert the author!");
+    }
+    Page.prototype[handlerName] = getEventHandler(event);
+}
+
+/**
+ * Used to dynamically restore the event
+ * @param event
+ */
+function restoreEvent(event) {
+    var handlerName = eventNames[event];
+
+    if (typeof events[event] !== 'function') {
+        throw new Error("NativeScript-globalEvent, this event was never hijacked to be removed!");
+    }
+    Page.prototype[handlerName] = events[event];
+}
+
+
+/**
+ * Find the Event in the callback list and return its index
+ * @param events
+ * @param callback
+ * @param thisArg
+ * @returns {number}
+ */
+function indexOfListener (events, callback, thisArg) {
+    var i;
+    if (thisArg) {
+        for (i = 0; i < events.length; i++) {
+            if (events[i].callback === callback && events[i].thisArg === thisArg) {
+                return i;
+            }
+        }
+    } else {
+        for (i = 0; i < events.length; i++) {
+            if (events[i].callback === callback) {
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+
+
+/**
+ * Setups a custom event handler for usaga
+ * @param eventType
+ * @returns {Function}
+ */
+function getEventHandler(eventType) {
+    return function () {
+        var eh = eventHandlers[eventType];
+        if (typeof eh !== 'undefined' && eh.length) {
+            for (var i = 0; i < eh.length; i++) {
+                var thisArg = eh[i].thisArg || this;
+                eh[i].callback.apply(thisArg, arguments);
+            }
+        }
+        events[eventType].apply(this, arguments);
+    };
+}
+
+
+
+
+
